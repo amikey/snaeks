c3ac794278ab42e15a46d8214b362a88f8f52aff
Paweł Stępień <pvl.staven@gmail.com>
2013-01-06 20:20:42 +0100

    Revert

--- path.pas
+++ path.pas
@@ -12,13 +12,11 @@ type
 		len: double;
 		dist: double;
 		
-		// index of the previous point in the NodePool.
-		prev: int;
+		prev: pWaypoint;
 	end;
 	
 	aWaypoint = array of Waypoint;
 	apWaypoint = array of pWaypoint;
-	aint = array of int;
 
 // pathTo returns the shortest path between the given points. The path doesn't include the starting point.
 // If no path can be found, the returned array has length 0.
@@ -32,59 +30,14 @@ uses tile;
 type
 	pPointQueue = ^PointQueue;
 	PointQueue = record
-		points: array of int;
+		points: apWayPoint;
 		
-		// first and last non-empty index of `points`
+		// first and last non-empty index of "points"
 		first, last: int;
 	end;
 	
 	pNodePool = ^NodePool;
-	NodePool = record
-		nodes: aWaypoint;
-		len: int;
-	end;
-
-function newNodePool(len: int; cap: int = -1): NodePool;
-var
-	ret: NodePool;
-	zeroval: Waypoint;
-	i: int;
-begin
-	if cap = -1 then cap := len;
-	ret.len := len;
-	
-	zeroval.x := 0;
-	zeroval.y := 0;
-	zeroval.len := 0;
-	zeroval.dist := 0;
-	zeroval.prev := -1;
-	
-	setLength(ret.nodes, cap);
-	for i := 0 to len-1 do begin
-		ret.nodes[i] := zeroval;
-	end;
-	
-	exit(ret);
-end;
-
-function NPappend(np: NodePool; node: Waypoint): NodePool;
-var
-	ret: NodePool;
-	i: int;
-begin
-	ret := np;
-	if ret.len + 1 > length(ret.nodes) then begin
-		setLength(ret.nodes, length(ret.nodes)*2);
-	end;
-	ret.nodes[ret.len] := node;
-	ret.len += 1;
-	exit(ret);
-end;
-
-function NPind(np: NodePool; ind: int): pWaypoint; inline;
-begin
-	exit(@(np.nodes[ind]));
-end;
+	NodePool = apWaypoint;
 
 function newPQ(): pPointQueue;
 var
@@ -128,14 +81,13 @@ begin
 	setLength(pq^.points, newasize);
 end;
 
-procedure PQinsert(pq: pPointQueue; point: int; all: NodePool);
+procedure PQinsert(pq: pPointQueue; point: pWaypoint);
 var
 	where, i: int;
 begin
 	where := pq^.first;
 	
-	while (where <= pq^.last) and
-			(NPind(all, point)^.dist >= NPind(all, pq^.points[where])^.dist) do begin
+	while (where <= pq^.last) and (point^.dist >= pq^.points[where]^.dist) do begin
 		where += 1;
 	end;
 	
@@ -144,29 +96,29 @@ begin
 	pq^.last += 1;
 end;
 
-procedure PQadd(pq: pPointQueue; point: int; all: NodePool);
+procedure PQadd(pq: pPointQueue; point: pWaypoint);
 var
 	newlen: int;
 begin
 	if PQlen(pq) < length(pq^.points) div 2 then begin
 		PQcompact(pq);
-		PQinsert(pq, point, all);
+		PQinsert(pq, point);
 		exit;
 	end;
 	
 	if pq^.last < high(pq^.points) then begin
-		PQinsert(pq, point, all);
+		PQinsert(pq, point);
 		exit;
 	end;
 	
 	newlen := length(pq^.points) * 2;
 	setLength(pq^.points, newlen);
-	PQinsert(pq, point, all);
+	PQinsert(pq, point);
 end;
 
-function PQremove(pq: pPointQueue): int;
+function PQremove(pq: pPointQueue): pWaypoint;
 var
-	ret: int;
+	ret: pWaypoint;
 begin
 	if PQlen(pq) < length(pq^.points) div 2 then begin
 		PQcompact(pq);
@@ -177,58 +129,73 @@ begin
 	exit(ret);
 end;
 
-function PQcontains(pq: pPointQueue; wp: int): boolean;
+function PQcontains(pq: pPointQueue; wp: pWaypoint): boolean;
 var
-	curr, currind: int;
+	curr: int;
+	currp: pWaypoint;
 begin
 	for curr := pq^.first to pq^.last do begin
-		currind := pq^.points[curr];
-		if currind = wp then exit(true);
+		currp := pq^.points[curr];
+		if currp = wp then exit(true);
 	end;
 	exit(false);
 end;
 
+// PQlookup finds a waypoint with the same coordinates as `wp` in the queue.
+// If no such point can be found, it returns `wp`.
+function PQlookup(pq: pPointQueue; wp: pWaypoint): pWaypoint;
+var
+	curr: int;
+	currp: pWaypoint;
+begin
+	for curr := pq^.first to pq^.last do begin
+		currp := pq^.points[curr];
+		if (currp^.x = wp^.x) and (currp^.y = wp^.y) then exit(currp);
+	end;
+	exit(wp);
+end;
 
 function estimate(fromp, top: Waypoint): double;
 begin
 	exit(sqrt((top.x-fromp.x)*(top.x-fromp.x) + (top.y-fromp.y)*(top.y-fromp.y)));
 end;
 
-function neighborsOf(wp: int; world: pWorldState; all: pNodePool): aint;
+function neighborsOf(wp: Waypoint; world: pWorldState; all: pNodePool): apWaypoint;
 const
-	xoff: array[1..4] of int = (-1,  1,  0,  0);
-	yoff: array[1..4] of int = ( 0,  0,  1, -1);
+	xoff: array[1..4] of int = ( 0,  1,  0, -1);
+	yoff: array[1..4] of int = (-1,  0,  1,  0);
 var
-	ret: aint;
-	seekwp: pWaypoint;
-	extantwp: int;
-	newwp: Waypoint;
-	x, y, i, j: int;
+	ret: apWaypoint;
+	seekwp, extantwp: pWaypoint;
+	x, y, i: int;
 	next: int;
 begin
 	next := 0;
 	
 	setLength(ret, 4);
 	for i := low(xoff) to high(xoff) do begin
-		x := NPind(all^, wp)^.x + xoff[i];
-		y := NPind(all^, wp)^.y + yoff[i];
+		x := wp.x + xoff[i];
+		y := wp.y + yoff[i];
 		if (not TMinBounds(world^.map, x, y)) or isOccupied(world, x, y) then continue;
-		extantwp := -1;
-		for j := 0 to all^.len-1 do begin
-			seekwp := NPind(all^, j);
+		
+		extantwp := nil;
+		for seekwp in all^ do begin
 			if (seekwp^.x = x) and (seekwp^.y = y) then begin
-				extantwp := j;
+				extantwp := seekwp;
 				break;
 			end;
 		end;
 		
-		if extantwp = -1 then begin
-			newwp.x := x;
-			newwp.y := y;
-			newwp.prev := -1;
-			all^ := NPappend(all^, newwp);
-			extantwp := all^.len - 1;
+		if extantwp = nil then begin
+			new(extantwp);
+			extantwp^.prev := nil;
+			setLength(all^, length(all^)+1);
+			all^[high(all^)] := extantwp;
 		end;
+		
+		extantwp^.x := x;
+		extantwp^.y := y;
+		//extantwp.len := wp.len + estimate(wp, newwp);
 		
 		ret[next] := extantwp;
 		next += 1;
@@ -238,20 +205,27 @@ begin
 	exit(ret);
 end;
 
-function trace(all: NodePool; endp: int): aWaypoint;
+function trace(all: NodePool; endp: pWaypoint): aWaypoint;
 var
 	curr: Waypoint;
 	ret: aWaypoint;
 	i, j: int;
 	dummy: int;
 begin
-	curr := NPind(all, endp)^;
+	curr := endp^;
 	setLength(ret, 0);
 	
-	while curr.prev <> -1 do begin
+	dummy := 0;
+	while curr.prev <> nil do begin
+		dummy += 1;
+		if dummy > 1000 then begin
+			writeln('path too long');
+			exit(ret);
+		end;
+		
 		setLength(ret, length(ret)+1);
 		ret[high(ret)] := curr;
-		curr := NPind(all, curr.prev)^;
+		curr := curr.prev^;
 	end;
 	
 	// Reverse the path.
@@ -273,27 +247,27 @@ function pathTo(world: pWorldState; fromp, top: Waypoint; cutoff: double = -1): 
 var
 	all: NodePool;
 	open, closed: pPointQueue;
-	wp, neighbor: int;
-	neighborptr: pWaypoint;
+	wp, neighbor: pWaypoint;
 	newneighbors: apWaypoint;
-	origin: pWaypoint;
 	path: aWaypoint;
 	newlen: double;
+	dummy: int;
 begin
-	all := newNodePool(1, 1000);
 	open := newPQ();
 	closed := newPQ();
 	
-	origin := NPind(all, 0);
-	origin^ := fromp;
-	origin^.len := 0;
-	origin^.dist := origin^.len + estimate(origin^, top);
-	origin^.prev := -1;
-	PQadd(open, 0, all);
+	setLength(all, 1);
+	new(all[0]);
+	wp := all[0];
+	wp^ := fromp;
+	wp^.len := 0;
+	wp^.dist := wp^.len + estimate(wp^, top);
+	wp^.prev := nil;
+	PQadd(open, wp);
 	
 	while PQlen(open) <> 0 do begin
-		if (cutoff >= 0) and (PQlen(closed) > 0) and
-				(NPind(all, closed^.points[closed^.last])^.len > cutoff) then begin
+		dummy := PQlen(open);
+		if (cutoff >= 0) and (PQlen(closed) > 0) and (closed^.points[closed^.last]^.len > cutoff) then begin
 			cleanupPQ(open);
 			cleanupPQ(closed);
 			setLength(path, 0);
@@ -301,24 +275,23 @@ begin
 		end;
 		
 		wp := PQremove(open);
-		if (NPind(all, wp)^.x = top.x) and (NPind(all, wp)^.y = top.y) then begin
+		if (wp^.x = top.x) and (wp^.y = top.y) then begin
 			path := trace(all, wp);
 			cleanupPQ(open);
 			cleanupPQ(closed);
 			exit(path);
 		end;
 		
-		PQadd(closed, wp, all);
-		for neighbor in neighborsOf(wp, world, @all) do begin
+		PQadd(closed, wp);
+		for neighbor in neighborsOf(wp^, world, @all) do begin
 			if PQcontains(closed, neighbor) then continue;
 			
-			neighborptr := NPind(all, neighbor);
-			newlen := NPind(all, wp)^.len + estimate(NPind(all, wp)^, neighborptr^);
-			if (not PQcontains(open, neighbor)) or (newlen <= neighborptr^.len) then begin
-				neighborptr^.prev := wp;
-				neighborptr^.len := newlen;
-				neighborptr^.dist := neighborptr^.len + estimate(neighborptr^, top);
-				if not PQcontains(open, neighbor) then PQadd(open, neighbor, all);
+			newlen := wp^.len + estimate(wp^, neighbor^);
+			if (not PQcontains(open, neighbor)) or (newlen <= neighbor^.len) then begin
+				neighbor^.prev := wp;
+				neighbor^.len := newlen;
+				neighbor^.dist := neighbor^.len + estimate(neighbor^, top);
+				if not PQcontains(open, neighbor) then PQadd(open, neighbor);
 			end;
 		end;
 	end;
